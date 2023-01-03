4fe1f085b55c426fe32d8e1b2f68cdf2c7d9b351
ldnhn <62146587+ldnhn@users.noreply.github.com>
2023-01-04 01:46:51 +0700

    soften cast shadow

--- Borrowed work/solar-system-threejs-master/js/SceneManager.js
+++ Borrowed work/solar-system-threejs-master/js/SceneManager.js
@@ -15,173 +15,183 @@ A SceneSubject represents one entity in the scene.
 The SceneManager usually contains multiple SceneSubjects.
 */
 
-
 function SceneManager(canvas) {
+  // scene setup
+  const screenDimensions = {
+    width: canvas.width,
+    height: canvas.height,
+  };
+  const scene = buildScene();
+  const renderer = buildRender(screenDimensions);
+  const camera = buildCamera(screenDimensions);
+  const sceneSubjects = createSceneSubjects(scene);
 
-    // scene setup
-    const screenDimensions = {
-        width: canvas.width,
-        height: canvas.height
-    }
-    const scene = buildScene();
-    const renderer = buildRender(screenDimensions);
-    const camera = buildCamera(screenDimensions);
-    const sceneSubjects = createSceneSubjects(scene);
+  const cameraControls = new MyCameraControls(camera, canvas);
 
-    const cameraControls = new MyCameraControls(camera, canvas);
+  scene.background = new THREE.Color("black");
 
-    scene.background = new THREE.Color('black');
+  //Picker
+  const pickPosition = { x: 0, y: 0 };
+  const pickHelper = new PickHelper();
+  clearPickPosition();
 
+  function buildScene() {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("#000");
 
-    //Picker
-    const pickPosition = { x: 0, y: 0 };
-    const pickHelper = new PickHelper();
-    clearPickPosition();
+    return scene;
+  }
 
-    function buildScene() {
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color("#000");
+  function buildRender({ width, height }) {
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+      alpha: true,
+    });
+    const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
+    renderer.setPixelRatio(DPR);
+    renderer.setSize(width, height);
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
 
-        return scene;
-    }
+    renderer.shadowMapEnabled = true;
 
-    function buildRender({ width, height }) {
-        const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
-        const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
-        renderer.setPixelRatio(DPR);
-        renderer.setSize(width, height);
+    return renderer;
+  }
 
-        renderer.gammaInput = true;
-        renderer.gammaOutput = true;
+  function buildCamera({ width, height }) {
+    const aspectRatio = width / height;
+    const fieldOfView = 60;
+    const nearPlane = 1;
+    const farPlane = 10000;
+    const camera = new THREE.PerspectiveCamera(
+      fieldOfView,
+      aspectRatio,
+      nearPlane,
+      farPlane
+    );
 
-        renderer.shadowMapEnabled = true;
+    camera.position.set(0, 300, 750);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-        return renderer;
-    }
+    return camera;
+  }
 
-    function buildCamera({ width, height }) {
-        const aspectRatio = width / height;
-        const fieldOfView = 60;
-        const nearPlane = 1;
-        const farPlane = 10000;
-        const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+  function createSceneSubjects(scene) {
+    const sceneSubjects = [
+      new SolarSystem(scene),
+      new AmbientLight(scene),
+      new SunLight(scene),
+      new Stars(scene),
+    ];
 
-        camera.position.set(0, 300, 750);
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
+    return sceneSubjects;
+  }
 
-        return camera;
-    }
+  // It is called by the main at every frame.
+  this.update = function (time) {
+    for (let i = 0; i < sceneSubjects.length; i++)
+      sceneSubjects[i].update(time);
 
-    function createSceneSubjects(scene) {
-        const sceneSubjects = [
-            new SolarSystem(scene),
-            new AmbientLight(scene),
-            new SunLight(scene),
-            new Stars(scene)
-        ];
+    let x = sceneSubjects[0];
+    pickHelper.pick(
+      pickPosition,
+      x.getAstrionomicalBodies(),
+      camera,
+      time,
+      scene
+    );
+    cameraControls.update();
+    renderer.render(scene, camera);
+  };
 
-        return sceneSubjects;
-    }
+  // Updates the aspect ratio of the camera and the size of the Renderer.
+  // It is called by the main each time the window is resized.
+  this.onWindowResize = function () {
+    const { width, height } = canvas;
 
-    // It is called by the main at every frame.
-    this.update = function (time) {
-        for (let i = 0; i < sceneSubjects.length; i++)
-            sceneSubjects[i].update(time);
+    screenDimensions.width = width;
+    screenDimensions.height = height;
 
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
 
-        let x = sceneSubjects[0];
-        pickHelper.pick(pickPosition, x.getAstrionomicalBodies(), camera, time, scene);
-        cameraControls.update();
-        renderer.render(scene, camera);
-    }
+    renderer.setSize(width, height);
+  };
 
-    // Updates the aspect ratio of the camera and the size of the Renderer. 
-    // It is called by the main each time the window is resized.
-    this.onWindowResize = function () {
-        const { width, height } = canvas;
+  // Picker
+  function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: ((event.clientX - rect.left) * canvas.width) / rect.width,
+      y: ((event.clientY - rect.top) * canvas.height) / rect.height,
+    };
+  }
 
-        screenDimensions.width = width;
-        screenDimensions.height = height;
+  function setPickPosition(event) {
+    const pos = getCanvasRelativePosition(event);
+    pickPosition.x = (pos.x / canvas.width) * 2 - 1;
+    pickPosition.y = (pos.y / canvas.height) * -2 + 1; // note we flip Y
+  }
 
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
+  function clearPickPosition() {
+    // unlike the mouse which always has a position
+    // if the user stops touching the screen we want
+    // to stop picking. For now we just pick a value
+    // unlikely to pick something
+    pickPosition.x = undefined;
+    pickPosition.y = undefined;
+  }
 
-        renderer.setSize(width, height);
-    }
+  window.addEventListener("dblclick", setPickPosition);
+  window.addEventListener("mouseout", clearPickPosition);
+  window.addEventListener("mouseleave", clearPickPosition);
 
+  // Mobile support
+  window.addEventListener(
+    "touchstart",
+    (event) => {
+      // prevent the window from scrolling
+      event.preventDefault();
+      setPickPosition(event.touches[0]);
+    },
+    { passive: false }
+  );
 
-    // Picker
-    function getCanvasRelativePosition(event) {
-        const rect = canvas.getBoundingClientRect();
-        return {
-            x: (event.clientX - rect.left) * canvas.width / rect.width,
-            y: (event.clientY - rect.top) * canvas.height / rect.height,
-        };
-    }
+  window.addEventListener("touchmove", (event) => {
+    setPickPosition(event.touches[0]);
+  });
 
-    function setPickPosition(event) {
-        const pos = getCanvasRelativePosition(event);
-        pickPosition.x = (pos.x / canvas.width) * 2 - 1;
-        pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
-    }
+  window.addEventListener("touchend", clearPickPosition);
 
-    function clearPickPosition() {
-        // unlike the mouse which always has a position
-        // if the user stops touching the screen we want
-        // to stop picking. For now we just pick a value
-        // unlikely to pick something
-        pickPosition.x = undefined;
-        pickPosition.y = undefined;
-    }
+  {
+    // Modal
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    var modal = document.getElementById("myModal");
 
-    window.addEventListener('dblclick', setPickPosition);
-    window.addEventListener('mouseout', clearPickPosition);
-    window.addEventListener('mouseleave', clearPickPosition);
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
 
-    // Mobile support
-    window.addEventListener('touchstart', (event) => {
-        // prevent the window from scrolling
-        event.preventDefault();
-        setPickPosition(event.touches[0]);
-    }, { passive: false });
-
-    window.addEventListener('touchmove', (event) => {
-        setPickPosition(event.touches[0]);
+    span.addEventListener("touchend", (event) => {
+      modal.style.display = "none";
     });
 
-    window.addEventListener('touchend', clearPickPosition);
+    // When the user clicks anywhere outside of the modal, close it (mobile)
+    window.addEventListener("touchend", (event) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
 
-
-
-    {
-        // Modal
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        var modal = document.getElementById("myModal");
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        span.addEventListener('touchend', (event) => {
-            modal.style.display = "none";
-        });
-
-
-        // When the user clicks anywhere outside of the modal, close it (mobile)
-        window.addEventListener('touchend', (event) => {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        });
-
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  }
 }
--- Borrowed work/solar-system-threejs-master/js/sceneSubjects/SunLight.js
+++ Borrowed work/solar-system-threejs-master/js/sceneSubjects/SunLight.js
@@ -1,15 +1,23 @@
 // Sun Light is a Point Light
 
 function SunLight(scene) {
-    const color = 0xFFFFFF;
-    const intensity = 0.5;
-    const sunLight = new THREE.PointLight(color, intensity);
-    sunLight.position.set(0, 0, 0);
-    sunLight.castShadow = true;
+  const color = 0xffffff;
+  const intensity = 0.2;
+  const sunLight = new THREE.PointLight(color, intensity);
+  sunLight.position.set(0, 0, 0);
+  sunLight.castShadow = true;
+  sunLight.shadow.mapSize.height = 1024;
+  sunLight.shadow.mapSize.width = 1024;
+  sunLight.shadowDarkness = 0.5;
 
-    scene.add(sunLight)
+  const intensity2 = 0.5;
+  const sunLight2 = new THREE.PointLight(color, intensity2);
+  sunLight2.position.set(0, 0, 0);
 
-    this.update = function () {
-        // do something
-    }
+  scene.add(sunLight);
+  scene.add(sunLight2);
+
+  this.update = function () {
+    // do something
+  };
 }
